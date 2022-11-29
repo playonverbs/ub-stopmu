dbc5a215bba8ae95d0760118c629a9397a6749d0
Niam Patel <naz1997@googlemail.com>
2022-11-29 12:15:40 +0000

    add fake data notebook and typed file

---
+++ fake_data/fake_data_typed.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import pandas as pd
+import pylandau
+from scipy.optimize import curve_fit
+
+import typing
+from collections.abc import Callable
+
+# invert Recombination Modified Box Model to get dE/dx from dQ/dx
+
+
+def ModBoxInverse(dqdx):
+    # argon density [g/cm^3]
+    rho = 1.396
+    # electric field [kV/cm]
+    efield = 0.273
+    # ionization energy [MeV/e]
+    Wion = 23.6 * (10 ** (-6))
+    fModBoxA = 0.93
+    fModBoxB = 0.562
+
+    dedx = (np.exp(fModBoxB * Wion * dqdx) - fModBoxA) / fModBoxB
+    return dedx
+
+def dEdx(x, elecgain):
+    dqdx = x["dqdx"] * elecgain
+    return ModBoxInverse(dqdx)
+
+def modulate_theory_mpv(
+    func:        Callable[[float], float],
+    # theory_mpvs: list[float],
+    egain_range: tuple[float, float],
+    egain_bins:  int
+) -> dict[str, list[float]]:
+    rr_ranges = np.linspace(100, 150, 10)
+    BINS = np.linspace(1, 6.0, 100)
+
+    rr_xs         = []
+    mpv_theory_xs = []
+    mpv_modded_xs = []
+
+    for n in xrange(len(rr_ranges) - 1):
+        rrmin = rr_ranges[n]
+        rrmax = rr_ranges[n+1]
+
+        if (n==1) or (n==3):
+            continue
+
+        rravg = 0.5 * (rrmax + rrmin)
+
+        rr_xs.append(rravg)
+        mpv_theory_xs.append(DPDX(ERange(rravg), 0.35, 105.6))
+
+    mpv_modded_xs = map(func, mpv_theory_xs)
+
+    return {"rr": rr_xs, "mpv_theory": mpv_theory_xs, "mpv_modulated": mpv_modded_xs}
+    
+def minimise_mpvs(
+    rr_vals:     list[float],
+    mpv_theory:  list[float],
+    mpv_data:    list[float],
+    egain_range: tuple[float, float],
+    egain_bins:  int
+) -> tuple[float, float]:
+    elec_gain = np.linspace(egain_range[0], egain_range[1], egain_bins)
+
+    for egain in elec_gain:
+        chilocal = 0.0
+
+        # a*X ~ Landau(a*mu * log)
+
+        for rr in rr_vals:
+            dftmp = datafit.query('rr > %i and rr < %i'%(rrmin,rrmax))
+        
+            dedx_v = dftmp['dedx'].values
+        
+            vals,bine = np.histogram(dedx_v,bins=BINS)
+            binc = 0.5*(bine[1:]+bine[:-1])
+            guess = [1.8,0.1,0.1,30.]
+    #         popt,popv = curve_fit(GL,binc,vals,p0=guess,bounds=([1.2,0.02,0.02,1],[4.0,0.3,0.3,200]))#,sigma=np.sqrt(vals),absolute_sigma=True)
+            popt,popv = curve_fit(GL,binc[0:FIT_TRUNC],vals[0:FIT_TRUNC],p0=guess,bounds=([1.2,0.02,0.02,1],[4.0,0.3,0.3,200]))#,sigma=np.sqrt(vals),absolute_sigma=True)
+
+            mpv_data = popt[0]
+            mpv_err  = np.sqrt(np.diag(popv))[0]
+
+            rr_xs.append(rravg)
+            mpv_theory_xs.append(DPDX(ERange(rravg), 0.35, 105.6))
+
+    return (1, -1)
+
+f: Callable[[float], float] = lambda x: x**2
